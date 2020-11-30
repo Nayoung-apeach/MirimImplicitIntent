6662d1a7c000a050c8c87de1d468c322866dacc5
Nayoung-apeach <s2019w04@e-mirim.hs.kr>
2020-11-30 19:13:08 +0900

    Set Button :D

--- app/src/main/java/kr/hs/emirim/s2019w04/mirimimplicitintent/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019w04/mirimimplicitintent/MainActivity.java
@@ -2,7 +2,11 @@ package kr.hs.emirim.s2019w04.mirimimplicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCall = findViewById(R.id.btn_call);
+        Button btnWeb = findViewById(R.id.btn_home_open);
+        Button btnMap = findViewById(R.id.btn_map);
+        Button btnSearch = findViewById(R.id.btn_search);
+        Button btnSms = findViewById(R.id.btn_sms);
+        Button btnCamera = findViewById(R.id.btn_camera);
+
+        btnCall.setOnClickListener(btnListener);
+        btnWeb.setOnClickListener(btnListener);
+        btnMap.setOnClickListener(btnListener);
+        btnSearch.setOnClickListener(btnListener);
+        btnSms.setOnClickListener(btnListener);
+        btnSearch.setOnClickListener(btnListener);
+
+
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = null;
+            switch (v.getId()){
+                case R.id.btn_call:
+                    break;
+                case R.id.btn_home_open:
+                    break;
+                case R.id.btn_map:
+                    break;
+                case R.id.btn_search:
+                    break;
+                case R.id.btn_sms:
+                    break;
+                case R.id.btn_camera:
+                    break;
+            }
+
+        }
+    };
 }
